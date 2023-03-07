dacf481f39b8b676fe088da2315b63dee33819bd
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 11:10:04 +0700

    create user header

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('user.home');
+        return view('user.home', [
+            'title' => 'Home page'
+        ]);
     }
 }
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,29 @@
+<header class="header">
+    <div class="header__container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="" width="120">
+        </div>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Service</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Blog</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="header__link">
+
+        </div>
+    </div>
+</header>
