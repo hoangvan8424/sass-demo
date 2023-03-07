3e3bd694893639f5b9bf9efbca9322e87ccb13b7
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 10:28:01 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        node: true,
+    },
+    "extends": [
+        "plugin:vue/recommended",
+        "eslint:recommended",
+        "@vue/prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
