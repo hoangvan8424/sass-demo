daedb7f5c9f8cad63b3d53071983c6f6b08d6481
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 10:36:30 +0700

    update config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
+        "parser": "babel-eslint",
     },
     "plugins": [
         "vue"
