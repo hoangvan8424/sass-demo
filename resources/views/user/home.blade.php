c99f640698b1b57dac800c173c086ee935edbcb0
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 17:27:17 +0700

    add mv

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.userApp')
 
 @section('content')
-<div class="container">
-
-</div>
+    <div class="home">
+        <div class="mv">
+            <div class="main__container mv__container">
+                <div class="mv__content">
+                    <h2 class="mv__title">
+                        An elite ecosystem of <br>
+                        <span>entrepreneurs.</span>
+                    </h2>
+                    <p class="mv__desc">We believe that technology has the power to make the world a better place and we invest our time and resources into entrepreneurs that are making an impact.</p>
+                    <a href="#" class="mv__btn">
+                        Get Started
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
