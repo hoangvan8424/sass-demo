80858a0e0c348e9c2a3c07d2198e9996f49ab2a3
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 11:13:13 +0700

    add font

--- resources/views/layouts/userApp.blade.php
+++ resources/views/layouts/userApp.blade.php
@@ -11,7 +11,9 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/userApp.scss', 'resources/js/userApp.js'])
