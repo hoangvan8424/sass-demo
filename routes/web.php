0c9f3cd5c77db14bd94dc67eb9c005bc35606c5d
ThaiHV <hoangvan181198@gmail.com>
2023-03-07 10:50:05 +0700

    create home page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
